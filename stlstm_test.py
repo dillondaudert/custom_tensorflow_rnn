e20885924fc51963631d31a8cd73f31a22cded5f
Dillon Daudert <dillongdaudert@gmail.com>
2018-04-29 12:18:48 -0400

    Test 0layer and 1layer state transitions do not change hidden state with identity kernel and zero bias

--- stlstm_test.py
+++ stlstm_test.py
@@ -46,6 +46,36 @@ class STLSTMTest(test.TestCase):
                 self.assertAllEqual(cell._st_kernels[layer].eval(), np.eye(num_units))
                 self.assertAllEqual(cell._st_biases[layer].eval(), np.zeros(num_units))
 
+    def test_cell_call(self, *args, **kwargs):
+        """Test that the state transition works properly on hidden-to-hidden states."""
+        batch_size = 2
+        num_units = 3
+        with self.test_session() as sess:
+            cell_0layer = stlstm.STLSTMCell(num_units, st_num_layers=0)
+            cell_1layer = stlstm.STLSTMCell(num_units, st_num_layers=1,
+                                            st_kernel_initializer=init_ops.identity_initializer())
+
+            init_state0 = cell_0layer.zero_state(batch_size, dtype=dtypes.float32)
+            init_state1 = cell_1layer.zero_state(batch_size, dtype=dtypes.float32)
+            # TODO: assert zero states aren't impacted by # st layers
+
+            # NOTE: 1-layer transition with identity and 0-bias should equal 0-layer (no transition)
+            inputs = random_ops.random_uniform([batch_size, 5])
+            h0, st0 = cell_0layer(inputs, init_state0)
+            h1, st1 = cell_1layer(inputs, init_state1)
+            variables.global_variables_initializer().run()
+
+            out0, newst0 = sess.run([h0, st0])
+            out1, newst1 = sess.run([h1, st1])
+
+            # NOTE: assert that a stlstm with 0 st layers output is the same as the new hidden state
+            self.assertAllEqual(out0, newst0[1])
+            # NOTE: assert the same for 1 layer st with an identity kernel and zero biases
+            self.assertAllEqual(out1, newst1[1])
+            # NOTE: assert that 0 layer and 1 layer new hidden states are the same
+            #self.assertAllEqual(newst0[1], newst1[1])
+
+
 
 if __name__ == '__main__':
     tf.test.main()
